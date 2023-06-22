b2ab691d5d3eacaaef103e0a8c6e157e4884dc1f
egeckmk <egeckmk@gmail.com>
2023-06-22 13:46:20 +0300

    ApiGateways structure set up

---
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Controllers/GatewayController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.ApiGateway.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class GatewayController : ControllerBase
+{
+    
+}
---
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot(builder.Configuration);
+builder.Configuration.AddJsonFile("Configurations/ocelot.json");
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.UseOcelot();
+app.Run();

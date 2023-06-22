20ff58a0ad889e15dd1f6d51e7047514ed24e5c9
egeckmk <egeckmk@gmail.com>
2023-06-22 14:50:10 +0300

    update system check api

--- src/Services/PhonebookService/PhonebookService.Api/Controllers/PhonebookController.cs
+++ src/Services/PhonebookService/PhonebookService.Api/Controllers/PhonebookController.cs
@@ -26,37 +26,7 @@ public class PhonebookController : ControllerBase
     [Route("systemCheck")]
     public async Task<IActionResult> SystemCheck()
     {
-        // SELECT cinfo.ContactContent AS [LOCATION], count(person.Id) as PERSON_COUNT, count(cinfo2.Id) as PHONE_COUNT 
-        // FROM PhoneBookApp.person.Person person
-        // inner join PhoneBookApp.person.ContactInfo cinfo on person.Id = cinfo.PersonId and cinfo.ContactType = 2
-        // LEFT join PhoneBookApp.person.ContactInfo cinfo2 on person.Id = cinfo2.PersonId and cinfo2.ContactType = 0 
-        // GROUP BY cinfo.ContactContent
-        
-        
-        var query = _context.Persons
-            .Join(_context.ContactInfos,
-                person => person.Id,
-                cinfo => cinfo.PersonId,
-                (person, cinfo) => new { person, cinfo })
-            .Where(x => x.cinfo.ContactType == ContactType.Location)
-            .GroupJoin(_context.ContactInfos,
-                x => x.person.Id,
-                cinfo2 => cinfo2.PersonId,
-                (x, cinfo2) => new { x, cinfo2 })
-            .SelectMany(x => x.cinfo2.DefaultIfEmpty(),
-                (x, cinfo2) => new { x.x.person, x.x.cinfo, cinfo2 })
-            .Where(x => x.cinfo2 == null || x.cinfo2.ContactType == 0)
-            .GroupBy(x => x.cinfo.ContactContent)
-            .Select(x => new ReportData
-            {
-                Location = x.Key,
-                PersonCount = x.Count(y => y.person != null),
-                PhoneCount = x.Count(y => y.cinfo2 != null)
-            });
-
-        var result = query.ToList();
-        
-        return Ok(result);
+        return Ok("System Online");
     }
 
     [HttpPost]
